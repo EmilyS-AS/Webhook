735f6ce29e59126c46952f9ab4de96438021287b
emily.salievski <emily.salievski@alliancesoftware.com.au>
2025-08-26 10:35:57 +1000

    comment

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
+# comment
+g
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
